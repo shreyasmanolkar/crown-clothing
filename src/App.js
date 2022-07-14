eee16c299cf746bf88601e85de17ee2ce639f553
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-07-14 12:28:37 +0530

    updated firebase authentication on site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component{
   constructor(props){
@@ -20,11 +21,22 @@ class App extends React.Component{
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+        if(userAuth){
+          const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
+          userRef.onSnapshot(snapShot => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+          });
+        } else {
+          this.setState({currentUser: userAuth});
+        }
+    });
   }
 
   componentWillUnmount(){
